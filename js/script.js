dcbf02b685cd0e18bfd8dc2bfbd2b4dc4d9e8f7e
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-14 10:45:25 +0200

    add todo

--- js/script.js
+++ js/script.js
@@ -1,22 +1,21 @@
+// * getDomElement
 const play = document.getElementById("play");
 const countdown = document.getElementById("countdown");
 const numbersList = document.getElementById("numbers-list");
 const instructions = document.getElementById("instructions");
 
-// * GetInputUser
-
+// * getInputUser
 const answerForms = document.getElementById("answers-form");
-const inputUserNumber = document.getElementsByTagName("input");
+const inputUserNumber = document.getElementsByTagName("input"); // ! ToDo_ changa with queryselectorAll
 const message = document.getElementById("message");
+const submitForm = document.getElementById("submit-form");
 
 // * globalVariables
-
-let remainingMs = 2000;
+let remainingMs = 10000;
 let coutndownIntervalId;
 let originNumber = [];
 
 // * handleCountdown
-
 const handleCountdown = () => {
   remainingMs -= 1000;
 
@@ -36,42 +35,36 @@ const timer = () => {
   coutndownIntervalId = setInterval(handleCountdown, 1000);
 };
 
+// * GenerateRandomNumbaer
 const randomNumber = (max, min) => {
   const value = Math.floor(Math.random() * (max - min + 1) + 1);
   return value;
 };
 
+// * Add Random number in unothered list
+// ! ToDo: change for with While
 const addNumbersInList = () => {
   const list = [];
+  while (originNumber.length < 5) {}
+
   for (i = 0; i < 5; i++) {
     const listItem = randomNumber(100, 1);
     originNumber.push(listItem);
-    list.push(`<li>${listItem}</li>`);
+    numbersList.innerHTML += `<li>${listItem}</li>`;
   }
-  console.log(list);
-
-  return list;
 };
 
-/**
- *
- * @param {number} originNumber
- * @param {number} userNumber
- * @returns number
- */
+// ! Todo: change duble for with array methods
 const compare = (originNumber, userNumber) => {
   let numberFuond = 0;
   for (let i = 0; i < originNumber.length; i++) {
     const currentOriginValue = originNumber[i];
-    // console.log(currentOriginValue);
 
     for (let y = 0; y < userNumber.length; y++) {
       const currentUserValue = userNumber[y];
-      //   console.log(currentUserValue);
 
       if (currentOriginValue == currentUserValue) {
         numberFuond += 1;
-        // console.log(numberFuond);
       }
     }
   }
@@ -81,35 +74,60 @@ const compare = (originNumber, userNumber) => {
 const getUserInput = (userInput) => {
   const numberInput = [];
 
-  for (let i = 0; i < inputUserNumber.length; i++) {
-    if (inputUserNumber[i].type.toLowerCase() == "number") {
+  for (let i = 0; i < userInput.length; i++) {
+    if (userInput[i].type.toLowerCase() == "number") {
       numberInput.push(parseInt(inputUserNumber[i].value));
     }
   }
   return numberInput;
 };
 
+const clearUserInput = (userInput) => {
+  for (let i = 0; i < userInput.length; i++) {
+    if (userInput[i].type.toLowerCase() == "number") {
+      userInput[i].value = "";
+    }
+  }
+};
+
+const playGame = () => {
+  remainingMs = 10000;
+  originNumber = [];
+  play.innerText = "Termina il gioco";
+  countdown.innerText = remainingMs / 1000;
+  countdown.classList.remove("d-none");
+  numbersList.classList.remove("d-none");
+  instructions.classList.remove("d-none");
+  message.innerText = "";
+  timer();
+  addNumbersInList();
+  clearUserInput(inputUserNumber);
+};
+
+const retry = () => {
+  location.reload();
+};
+
+const endGame = () => {
+  play.innerText = "Inizia il gioco";
+  countdown.classList.add("d-none");
+  numbersList.classList.add("d-none");
+  instructions.classList.add("d-none");
+  clearInterval(coutndownIntervalId);
+  coutndownIntervalId = undefined;
+  answerForms.classList.add("d-none");
+};
+
 play.addEventListener("click", () => {
   console.warn(`Valore: ${play.textContent}`);
   if (play.textContent === "Inizia il gioco") {
-    remainingMs = 2000;
-    play.innerText = "Termina il gioco";
-    countdown.innerText = remainingMs / 1000;
-    countdown.classList.remove("d-none");
-    numbersList.classList.remove("d-none");
-    instructions.classList.remove("d-none");
-    timer();
-    numbersList.innerHTML = addNumbersInList();
+    playGame();
   } else {
-    play.innerText = "Inizia il gioco";
-    countdown.classList.add("d-none");
-    numbersList.classList.add("d-none");
-    instructions.classList.add("d-none");
-    clearInterval(coutndownIntervalId);
-    coutndownIntervalId = undefined;
-    answerForms.classList.add("d-none");
+    endGame();
   }
 });
+
+// ! ToDo: add array guessed numbers
 answerForms.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -117,5 +135,20 @@ answerForms.addEventListener("submit", (event) => {
   const howManyNumberFound = compare(originNumber, UserInput);
   console.log(UserInput);
   console.log(howManyNumberFound);
-  message.innerText = `hai indovinato ${howManyNumberFound} numeri!`;
+  if (howManyNumberFound === 5) {
+    message.innerText = `Complimenti hai indovinato tutti i numeri!`;
+  } else {
+    message.innerText = `hai indovinato ${howManyNumberFound} numeri!`;
+    console.log(`hai indovinato ${howManyNumberFound} numeri!`);
+    submitForm.innerText = "Ritenta";
+    console.log("ritenta");
+  }
+});
+
+submitForm.addEventListener("click", () => {
+  if (submitForm.textContent === "Ritenta") {
+    retry();
+  } else if (submitForm.textContent === "Ricomincia") {
+    playGame();
+  }
 });
